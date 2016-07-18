2d82a124e3108a49f494bee38c9704f13ad6b9b4
Patrick Todd <patrick.a.todd@gmail.com>
2016-07-18 18:54:30 -0400

    modified html to add form button, and parameters for data being passed from python

--- app/controllers/Welcome.py
+++ app/controllers/Welcome.py
@@ -7,34 +7,19 @@
     Create a controller using this template
 """
 from system.core.controller import *
+from flask import Flask, render_template, request, redirect, session
+import string
+import random
 
 class Welcome(Controller):
     def __init__(self, action):
         super(Welcome, self).__init__(action)
-        """
-            This is an example of loading a model.
-            Every controller has access to the load_model method.
-        """
-        self.load_model('WelcomeModel')
-        self.db = self._app.db
 
-        """
-        
-        This is an example of a controller method that will load a view for the client 
-
-        """
-   
     def index(self):
-        """
-        A loaded model is accessible through the models attribute 
-        self.models['WelcomeModel'].get_users()
-        
-        self.models['WelcomeModel'].add_message()
-        # messages = self.models['WelcomeModel'].grab_messages()
-        # user = self.models['WelcomeModel'].get_user()
-        # to pass information on to a view it's the same as it was with Flask
-        
-        # return self.load_view('index.html', messages=messages, user=user)
-        """
-        return self.load_view('index.html')
+        if not session.has_key('count'):
+            session['count'] = 1
+        else:
+            session['count'] += 1
+        word = ''.join(random.SystemRandom().choice(string.ascii_uppercase + string.digits) for _ in range(5,20))
+        return self.load_view('index.html', count=session['count'], word=word)
 
